4e39549a3a67998fe6455f1edca51f9c52798c27
J Balabas <jbalabas+git@outlook.com>
2013-02-09 20:18:54 -0600

    simple eclipse project

---
+++ src/crashburn/simple/hello/HelloCity.java
@@ -0,0 +1,14 @@
+package crashburn.simple.hello;
+
+public class HelloCity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello city!");
+
+	}
+
+}
